68445df099629c040799ad7f5b655672f1b8e944
99003575 <78539650+99003575@users.noreply.github.com>
2021-02-06 14:04:54 +0530

    Create unit.c

---
+++ Test/unit.c
@@ -0,0 +1,64 @@
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
+
+/* Modify these two lines according to the project */
+#include <calculator_operations.h>
+#define PROJECT_NAME    "mahavira_team1_calculator"
+
+/* Prototypes for all the test functions */
+
+void test_subtraction(void);
+
+void test_factorial(void);
+
+
+/* Start of the application test */
+int main() {
+/* Note: Do not edit START*/
+  /*Initialize and setup the Test Framework */
+  if (CUE_SUCCESS != CU_initialize_registry())
+    return CU_get_error();
+  CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
+/* Note: Do not edit END */
+  
+  
+  /* Add your test functions in this format for testing*/
+  
+  CU_add_test(suite, "subtract", test_subtract);
+  
+  CU_add_test(suite, "factorial", test_factorial);
+ 
+
+
+
+/* Note: Do not edit START*/
+  /* Setup Test Framework to output the result to Screen */
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+  
+  /* run the unit test framework*/
+  CU_basic_run_tests();
+  
+  /* Cleaning the Resources used by Unit test framework */
+  CU_cleanup_registry();
+/* Note: Do not edit END */
+  return 0;
+}
+
+/* Write all the test functions */ 
+
+void test_subtract(void) {
+  CU_ASSERT(-3 == subtract(0, 3));
+  
+  /* Dummy fail*/
+  CU_ASSERT(1 == subtract(1000, 900));
+}
+
+
+void test_factorial(void) {
+  CU_ASSERT(6 == factorial(3));
+  CU_ASSERT(-1 == factorial(-12))
+  
+  /* Dummy fail*/
+  CU_ASSERT(10 == factorial(97));
+}
+
